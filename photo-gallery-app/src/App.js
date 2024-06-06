23c1568b34fd06829f3eff726c50e8e3e4b6b535
Madhav Singh <madhavnegi1@gmail.com>
2024-06-06 18:01:07 +0000

    modified:   photo-gallery-app/src/App.js 	new file:   photo-gallery-app/src/components/Header.js 	new file:   photo-gallery-app/src/content/data.js 	new file:   photo-gallery-app/src/modal/modal.js

--- photo-gallery-app/src/App.js
+++ photo-gallery-app/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { data } from "./content/data";
+import Modal from "./modal/Modal";
+import Header from "./components/Header";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedContent, setSelectedContent] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {showModal && (
+        <Modal
+          setShowModal={setShowModal}
+          setSelectedContent={setSelectedContent}
+          content={selectedContent}
+          data={data}
+        />
+      )}
+      <div style={styles.main}>
+        <Header
+          title="Photo Gallery"
+          subtitle="A simple photo gallery app built by stackup-username"
+        />
+        <div style={styles.container}>
+          {data.map((image_data, index) => {
+            return (
+              <div
+                style={styles.imageContainer}
+                onClick={() => {
+                  setShowModal(true);
+                  setSelectedContent(image_data);
+                }}
+              >
+                <img style={styles.image} src={image_data.url} />
+                <div style={styles.imageDescription}>
+                  <p style={styles.imageDescriptionText}>{image_data.name}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
